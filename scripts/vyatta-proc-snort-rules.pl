1b8e16a7a1715ee8deee9fc9c28dc5af68a63cd1
Stig Thormodsrud <stig@vyatta.com>
2010-04-14 15:38:08 -0700

    Add copyright.

--- scripts/vyatta-proc-snort-rules.pl
+++ scripts/vyatta-proc-snort-rules.pl
@@ -1,4 +1,27 @@
 #!/usr/bin/perl
+#
+# Module: vyatta-proc-snort-rules.pl
+# 
+# **** License ****
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# This code was originally developed by Vyatta, Inc.
+# Portions created by Vyatta are Copyright (C) 2008-2010 Vyatta, Inc.
+# All Rights Reserved.
+# 
+# Author: An-Cheng Huang
+# Date: April 2008
+# Description: process snort rules
+# 
+# **** End License ****
+#
 
 use strict;
 use Getopt::Long;
--- scripts/vyatta-update-inspection.pl
+++ scripts/vyatta-update-inspection.pl
@@ -1,5 +1,27 @@
 #!/usr/bin/perl
-
+#
+# Module: vyatta-update-inspection.pl
+# 
+# **** License ****
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# This code was originally developed by Vyatta, Inc.
+# Portions created by Vyatta are Copyright (C) 2008-2010 Vyatta, Inc.
+# All Rights Reserved.
+# 
+# Author: An-Cheng Huang
+# Date: April 2008
+# Description: update content inspection
+# 
+# **** End License ****
+#
 use strict;
 use lib "/opt/vyatta/share/perl5";
 use Vyatta::Snort::Config;
