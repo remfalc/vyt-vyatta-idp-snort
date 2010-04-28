062a23178768152063d388d221dc40567eadcd0d
Stig Thormodsrud <stig@vyatta.com>
2010-04-28 12:24:13 -0700

    Fix 5562: ips logs alerts even for pass rules.

--- lib/Vyatta/Snort/Config.pm
+++ lib/Vyatta/Snort/Config.pm
@@ -405,7 +405,7 @@ EOD
 my $rule_pass_def =<<EOD;
 {
   type pass
-$output_def
+  output log_null
 }
 EOD
 
@@ -444,7 +444,7 @@ EOS
 include clamav.config
 
 ## set output module
-output alert_unified: filename snort-unified.alert, limit 1
+output alert_fast: alert
 
 EOS
 
